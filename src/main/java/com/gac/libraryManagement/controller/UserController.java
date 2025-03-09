72d3b9b7369aa98d0384cd11120a66ebb9e2820f
madhu2628 <maduradevi3110@gmail.com>
2025-03-09 23:57:31 +0530

    service control repository

--- src/main/java/com/gac/libraryManagement/controller/UserController.java
+++ src/main/java/com/gac/libraryManagement/controller/UserController.java
@@ -5,14 +5,12 @@ import com.gac.libraryManagement.repository.UserRepository;
 import com.gac.libraryManagement.service.UserDetailsService;
 import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class UserController {
     private final UserDetailsService userDetailsService;
 
@@ -30,7 +28,7 @@ public class UserController {
         return ResponseEntity.ok(userDetailsService.registerNewUser(userDetails));
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<UserDetails> login(@RequestBody UserDetails userDetails) {
         if (userDetails.getEmailId() != null && userDetails.getPassword() != null) {
             return ResponseEntity.ok(userDetailsService.login(userDetails));
